3c121962f1bc9dbe8352a73af6ca7caaf54c056b
Victor S <victor.sogamoso.05@gmail.com>
2025-03-18 10:05:31 -0500

    borrado de headers de git

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,6 @@ import routes from './routes/upload.routes';
 class App {
     public app: Application;
 
-<<<<<<< HEAD
     constructor() {
         console.log('App constructor called');
         this.app = express();
@@ -24,23 +23,5 @@ class App {
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: true }));
       }
-=======
-    constructor(){
-        this.app = express();
-        this.config();
-        this.routes();
-    }
-
-    private config(): void {
-        //Cors
-        this.app.use(cors());
-        //Parseo
-        this.app.use(express.json());
-    }
-
-    private routes(){
-        this.app.use('api/upload', routes);
-    }
->>>>>>> 1238d59558643b1305fd11d6217fa0fc62254847
 }
 export default new App().app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,6 @@
 import app from './app';
 
-<<<<<<< HEAD
 const puerto = process.env.PORT || 4000;
-=======
-const puerto = process.env.PORT || 3000;
->>>>>>> 1238d59558643b1305fd11d6217fa0fc62254847
 
 app.listen(puerto, ()=>{
     console.log(`Servidor corriendo en el puerto: ${puerto}`);
